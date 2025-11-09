b8a05a5e033812123eeaefd20f01d5ba9750a3f5
CPA FERRO E AÇO <cpaferroeaco@gmail.com>
2025-11-09 20:52:06 +0000

    File changes

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -1,9 +1,17 @@
 import * as React from "react"
 
-const TOAST_LIMIT = 3
-const TOAST_REMOVE_DELAY = 5000
+const TOAST_LIMIT = 1
+const TOAST_REMOVE_DELAY = 1000000
+
+const actionTypes = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+}
 
 let count = 0
+
 function genId() {
   count = (count + 1) % Number.MAX_VALUE
   return count.toString()
