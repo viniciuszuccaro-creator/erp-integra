21fb05ff74f1ae9e494e5008b96394c496f8f786
CPA FERRO E AÇO <cpaferroeaco@gmail.com>
2025-11-20 19:55:26 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -212,7 +212,7 @@ function LayoutContent({ children, currentPageName }) {
               </div>
               <div>
                 <h2 className="font-bold text-xl text-slate-900">ERP Zuccaro</h2>
-                <p className="text-xs text-slate-500">V21.2.2 • FASE 2 ✅ 100% • 89W</p>
+                <p className="text-xs text-slate-500">V21.2.3 • FASE 2 ✅ 100% • 90W</p>
               </div>
             </div>
           </SidebarHeader>
