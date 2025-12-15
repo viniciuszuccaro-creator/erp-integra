b7469f6ef71f1c9ac7e45e5adb666c9897fb6397
CPA FERRO E AÇO <cpaferroeaco@gmail.com>
2025-12-15 18:10:51 +0000

    File changes

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -8,7 +8,7 @@ const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-slate-300 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white data-[state=checked]:border-blue-600",
       className
     )}
     {...props}
@@ -20,7 +20,8 @@ const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+Checkbox.displayName = "Checkbox";
 
 export { Checkbox };
 export default Checkbox;
